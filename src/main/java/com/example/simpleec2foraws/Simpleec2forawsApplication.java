3e9dcaef89809e85b0813dd2115818fcce6bff99
Dmitrii_Korolev <korwork@mail.ru>
2019-11-26 12:06:12 -0500

    base classes

---
+++ src/main/java/com/example/simpleec2foraws/Simpleec2forawsApplication.java
@@ -0,0 +1,13 @@
+package com.example.simpleec2foraws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Simpleec2forawsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Simpleec2forawsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/simpleec2foraws/Simpleec2forawsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.simpleec2foraws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Simpleec2forawsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
